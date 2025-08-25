477bda84857576c297b17d263de6356584dc7247
MohammadrezaNazirii <mohammadrezanaziri0@gmail.com>
2025-08-25 19:00:06 +0330

    test

--- images.py
+++ images.py
@@ -6,30 +6,30 @@ from io import BytesIO
 ##################################################################
 
 
-def extract_images(uploaded_file):
-    uploaded_file.seek(0)
-    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-    image_list = []
+# def extract_images(uploaded_file):
+#     uploaded_file.seek(0)
+#     doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+#     image_list = []
 
-    for page_num in range(len(doc)):
-        page = doc[page_num]
-        images = page.get_images(full=True)
+#     for page_num in range(len(doc)):
+#         page = doc[page_num]
+#         images = page.get_images(full=True)
 
-        for img_index, img in enumerate(images):
-            xref = img[0]
-            pix = fitz.Pixmap(doc, xref)
+#         for img_index, img in enumerate(images):
+#             xref = img[0]
+#             pix = fitz.Pixmap(doc, xref)
 
-            # Convert CMYK or grayscale images to RGB
-            if pix.n - pix.alpha >= 4:  # CMYK
-                pix = fitz.Pixmap(fitz.csRGB, pix)
-            elif pix.n == 1:  # grayscale
-                pix = fitz.Pixmap(fitz.csRGB, pix)
+#             # Convert CMYK or grayscale images to RGB
+#             if pix.n - pix.alpha >= 4:  # CMYK
+#                 pix = fitz.Pixmap(fitz.csRGB, pix)
+#             elif pix.n == 1:  # grayscale
+#                 pix = fitz.Pixmap(fitz.csRGB, pix)
 
-            img_bytes = pix.tobytes("png")  # get PNG bytes
-            image_list.append(BytesIO(img_bytes))
-            pix = None  # free memory
+#             img_bytes = pix.tobytes("png")  # get PNG bytes
+#             image_list.append(BytesIO(img_bytes))
+#             pix = None  # free memory
 
-    return image_list
+#     return image_list
 
 
 
@@ -86,22 +86,22 @@ def extract_images(uploaded_file):
 #     return image_list
 
 
-# def extract_images(uploaded_file):
-#     #pdf_document = fitz.open(pdf_path)
-#     uploaded_file.seek(0)
-#     doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-#     image_bytes_list = []
+def extract_images(uploaded_file):
+    #pdf_document = fitz.open(pdf_path)
+    uploaded_file.seek(0)
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    image_bytes_list = []
 
-#     for page_num in range(len(doc)):
-#         page = doc[page_num]
-#         images = page.get_images(full=True)
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        images = page.get_images(full=True)
 
-#         for img_index, img in enumerate(images):
-#             xref = img[0]
-#             base_image = doc.extract_image(xref)
-#             image_bytes = base_image["image"]
-#             # store as BytesIO object
-#             image_bytes_io = BytesIO(image_bytes)
-#             image_bytes_list.append(image_bytes_io)
+        for img_index, img in enumerate(images):
+            xref = img[0]
+            base_image = doc.extract_image(xref)
+            image_bytes = base_image["image"]
+            # store as BytesIO object
+            image_bytes_io = BytesIO(image_bytes)
+            image_bytes_list.append(image_bytes_io)
 
-#     return image_bytes_list
+    return image_bytes_list
